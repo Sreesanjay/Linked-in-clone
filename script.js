ffbf735b8fb3a15e92549f0fc4b969977c923a37
Sreesanjay <sreesanjay7592@gmail.com>
2023-07-24 20:08:56 +0530

    completed post section

--- script.js
+++ script.js
@@ -40,3 +40,58 @@ function scaleBusinessCard(){
         document.querySelector('.for-business').style.opacity ='0.6';
     }
 }
+function exparea(){
+    const ext=document.getElementById('right-sec-card');
+    const up=document.querySelector('.up');
+    const dwn=document.querySelector('.dwn');
+    ext.style.maxHeight='100%';
+    up.style.display='block';
+    dwn.style.display='none';
+}
+function compressarea(){
+    const ext=document.getElementById('right-sec-card');
+    const up=document.querySelector('.up');
+    const dwn=document.querySelector('.dwn');
+    ext.style.maxHeight='320px';
+    up.style.display='none';
+    dwn.style.display='block';
+}
+function seeMore(ele){
+    const fc=ele.parentNode;
+    const ext=fc.querySelector('.ext-cap');
+    console.log(ext);
+    const more=fc.querySelector('.s-more');
+    const less=fc.querySelector('.s-less');
+    ext.style.display='block';
+    more.style.display='none';
+    less.style.display='block';
+}
+function seeLess(ele){
+    const fc=ele.parentNode;
+    const ext=fc.querySelector('.ext-cap');
+    const more=fc.querySelector('.s-more');
+    const less=fc.querySelector('.s-less');
+    ext.style.display='none';
+    more.style.display='block';
+    less.style.display='none';
+}
+function expandProfArea(){
+    const expSec1=document.querySelector('.left-sec-expand-area');
+    const expSec2=document.querySelector('.left-sec-card-2');
+    const expup=document.querySelector('.left-sec-expand');
+    const expdwn=document.querySelector('.left-sec-comp');
+    expSec1.style.display='block';
+    expSec2.style.display='block';
+    expup.style.display='none';
+    expdwn.style.display='block'
+}
+function compressProfArea(){
+    const expSec1=document.querySelector('.left-sec-expand-area');
+    const expSec2=document.querySelector('.left-sec-card-2');
+    const expup=document.querySelector('.left-sec-expand');
+    const expdwn=document.querySelector('.left-sec-comp');
+    expSec1.style.display='none';
+    expSec2.style.display='none';
+    expup.style.display='block';
+    expdwn.style.display='none'
+}
